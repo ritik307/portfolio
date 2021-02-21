82f536bb92f006d985615d12646df54b33c651bf
Ritik <ritikpr307@gmail.com>
2021-02-21 18:36:30 +0530

    added content in the education route

--- src/components/routes/Education.js
+++ src/components/routes/Education.js
@@ -32,7 +32,7 @@ class Education extends React.Component {
           cname: "Bharati Vidyapeeth's Institute of Computer Applications and Management",
           date: "2019-2022",
           desc:
-            "To overcome the problem of food wastage, Onlymeal provides a two way interaction between those with a food surplus and those looking for food. The data collected from this app includes nature of the food, date of expiry, location etc. This data when aggregated can be very useful for NGOs to connect the locations with surplus with those that are in need of the food.",
+            "Active member and an organiser of the HackBVICAM hackathon.",
           site: "http://bvicam.in/",
         },
         {
@@ -41,7 +41,7 @@ class Education extends React.Component {
           cname: "Ambedkar Institute of Technology",
           date: "2016-2019",
           desc:
-            "While studying one might want to draw on the same page in order to make others or self understand the concept in a deeper manner. It is a extension that allows user to doodle over the active web page. The user can doodle with different colours,shapes and strokes.",
+            "Particiated in SIH(Smart India Hackathon)2019 and other hackathons.",
 
           site: "http://www.ambp.in/ ",
         },
@@ -51,19 +51,11 @@ class Education extends React.Component {
           cname: "Lovely Public Sr. Sec. School",
           date: "2002-2016",
           desc:
-            "Visually impaired citizens face a lot of problems with the CAPTCHA authentication process. In some of the applications OTP is given as an alternative but not with the rest of the apps. In some of the apps audio captcha is provided but many citizens find it difficult to decipher. To overcome this problem designed a CAPTCHA using environmental sounds.",
+            "Participated in various inter-school competitions and was also an active member of annual cultural fest.",
 
           site: "http://www.lpsschool.com/",
         },
-        // {
-        //   id: 3,
-        //   name: "High Schooling",
-        //   date: "Apr 2020-May 2020",
-        //   desc:
-        //     "A blogging web Application that allowes user to post thier blogs. Application allows user to create their account. User can add comment to others post. Only registered users are allowed to post the content on the application. Registered users can edit/delete their post.",
-
-        //   site: "http://www.lpsschool.com/",
-        // },
+        
       ],
     };
   }
